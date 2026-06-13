9523a053755607c17d5b0b51ecc26e5d25da589d
Shiva Singh <shivasing6515@gmail.com>
2026-06-13 21:04:06 +0530

    Implement Playable interface in Saxophone class

--- Week_2/OOPS/music/wind/Saxophone.java
+++ Week_2/OOPS/music/wind/Saxophone.java
@@ -1,1 +1,14 @@
-
+// Topic        : Interface + Packages
+// Language     : Java
+ 
+package music.wind;
+ 
+import music.Playable;
+ 
+public class Saxophone implements Playable {
+ 
+    @Override
+    public void play() {
+        System.out.println("Playing Saxophone...");
+    }
+}
