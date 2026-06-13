0827a1b5f871fab506769de2a5ef77409a7b5d1e
Shiva Singh <shivasing6515@gmail.com>
2026-06-13 20:59:30 +0530

    Create Playable interface in music package
    Added Playable interface with play method.

--- Week_2/OOPS/music/Playable.java
+++ Week_2/OOPS/music/Playable.java
@@ -1,1 +1,9 @@
-
+// Problem Name : Playable Interface - Package Demo
+// Topic        : Interface + Packages
+// Language     : Java
+ 
+package music;
+ 
+public interface Playable {
+    void play();
+}
